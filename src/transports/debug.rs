2ecefdfa41c2ccf261812698b73cbf22678a91a0
Nic Hartley <nic@cybers.eco>
2019-12-28 22:59:55 -0500

    fix: correct Debug::send output

--- src/transports/debug.rs
+++ src/transports/debug.rs
@@ -24,16 +24,19 @@ impl super::Transport for Debug {
 
   fn send(&mut self, blob: Vec<u8>) -> Result<(), Self::Fail> {
     println!("Debug {} sent {} bytes. In hex:", self.prefix, blob.len());
+    // most of the message
     for s in 0..blob.len() / 16 {
-      let end = if s + 16 < blob.len() {
-        s + 16
-      } else {
-        blob.len() - 1
-      };
-      let bytes = blob[s..end].iter().map(|b| format!("{:x}", b)).join("");
-      println!("{:x}: {}", s, bytes);
-    }
-    Ok(())
+      let bytes = blob[s..s + 16].iter().map(|b| format!("{:x}", b)).join("");
+      println!("{:4x}: {}", s, bytes);
+  }
+  // last line
+  let overhang = blob.len() % 16;
+  if overhang > 0 {
+      let oh_start = blob.len() - overhang;
+      let bytes = blob[oh_start..].iter().map(|b| format!("{:02x}", b)).join("");
+      println!("{:4x}: {}", oh_start, bytes);
+  }
+  Ok(())
   }
 
   fn recv(&mut self) -> Result<Vec<Vec<u8>>, Self::Fail> {
