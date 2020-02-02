94e27b16d8f855fa52ad13ed7502938286c969d2
Nic Hartley <nic@cybers.eco>
2020-02-02 08:38:33 -0600

    example: tcp send/receive pair

---
+++ mesher_standard/examples/tcp-recv.rs
@@ -0,0 +1,38 @@
+use mesher::prelude::*;
+use mesher_standard::TCP;
+
+fn main() {
+  let mut args = std::env::args().skip(1);
+  let sock = args
+    .next()
+    .unwrap_or("[::1]:18540".to_owned());
+
+  println!("Listening for data on {}", sock);
+
+  let mut m = Mesher::unsigned(vec![SecretKey::of("receiver")]);
+  m.add_transport::<TCP>("tcp").expect("Failed to add required transport");
+  m.listen_on(&format!("tcp:{}", sock)).expect("Failed to add listener for messages");
+
+  loop {
+    let recvd = m.recv().expect("Failed to receive messages");
+    for msg in recvd {
+      let contents = msg.contents();
+      match std::str::from_utf8(contents) {
+        Ok(s) if s.chars().all(|c| c.is_ascii_graphic() || c.is_ascii_whitespace()) => {
+          println!("Text message received ({} chars):", s.len());
+          println!("{}", s);
+        }
+        _ => {
+          println!("Binary message received ({} bytes):", contents.len());
+          for (i, byte) in contents.iter().enumerate() {
+            print!("{:02x} ", byte);
+            if i % 8 == 7 {
+              println!();
+            }
+          }
+        }
+      };
+    }
+    std::thread::sleep(std::time::Duration::from_millis(100));
+  }
+}
---
+++ mesher_standard/examples/tcp-send.rs
@@ -0,0 +1,25 @@
+use std::{
+  net::ToSocketAddrs,
+  io::{stdin, Read},
+};
+
+use mesher::prelude::*;
+use mesher_standard::TCP;
+
+fn main() {
+  let mut args = std::env::args().skip(1);
+  let sock = args
+    .next()
+    .unwrap_or("[::1]:18540".to_owned());
+
+  println!("Enter the data to send to {}, then send EOF when done.", sock);
+
+  let mut data = vec![];
+  stdin().lock().read_to_end(&mut data).expect("Failed to read from STDIN");
+
+  println!("\n\n---\nSending {} bytes...", data.len());
+  let mut m = Mesher::unsigned(vec![SecretKey::of("who cares")]);
+  m.add_transport::<TCP>("tcp").expect("Failed to add TCP transport");
+  m.send(&data, Route::to(&PublicKey::of("receiver"), &format!("tcp:{}", sock))).expect("Failed to send data");
+  println!("Sent! Did you see it get received?");
+}
