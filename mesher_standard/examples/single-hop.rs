a6a2d0fcc09bcd8d81382c0ddc9361814a88c013
Nic Hartley <nic@cybers.eco>
2020-02-01 23:29:53 -0600

    feat: send all, then recv all, isntead of one at a time

--- mesher_standard/examples/single-hop.rs
+++ mesher_standard/examples/single-hop.rs
@@ -10,39 +10,48 @@ fn make_sender() -> Mesher {
   m
 }
 
-fn make_receiver() -> (Mesher, Route) {
+fn make_receiver() -> Mesher {
   let mut m = Mesher::unsigned(vec![SecretKey::of("receiver")]);
   m.add_transport::<TCP>("tcp")
     .expect("Failed to add transport");
   m.listen_on("tcp:[::1]:18540")
     .expect("Failed to listen on port");
-  (m, Route::to(&PublicKey::of("receiver"), "tcp:[::1]:18540"))
+  m
 }
 
-const MESSAGES: &[&str] = &["Hello", "Goodbye"];
+const MESSAGES: &[&str] = &["Hello", "This is a TCP demo", "Goodbye"];
 
 fn main() {
   let mut m1 = make_sender();
-  let (mut m2, path) = make_receiver();
+  let mut m2 = make_receiver();
+  let path = Route::to(&PublicKey::of("receiver"), "tcp:[::1]:18540");
 
   for message in MESSAGES {
     m1.send(message.as_bytes(), path.clone())
       .expect("Failed to send");
     println!("Message sent: {}", message);
-    loop {
-      let recvd = m2.recv().expect("Failed to recv");
-      match recvd.first() {
-        Some(s) => {
-          let contents =
-            std::str::from_utf8(s.contents()).expect("Invalid UTF-8");
-          println!("Message received: {}", contents);
-          break;
-        }
-        None => {
-          println!("No message gotten yet...");
-          sleep(Duration::from_millis(10));
-        }
+  }
+
+  let mut to_read = MESSAGES.len();
+  loop {
+    let recvd = m2.recv().expect("Failed to recv");
+    if recvd.is_empty() {
+      println!("No message gotten yet...");
+      sleep(Duration::from_millis(10));
+    } else {
+      to_read -= recvd.len();
+      for msg in recvd {
+        let contents =
+          std::str::from_utf8(msg.contents()).expect("Invalid UTF-8");
+        println!("Message received: {}", contents);
+      }
+      if to_read == 0 {
+        break;
+      } else {
+        println!("Only received some. Waiting for rest...");
+        sleep(Duration::from_millis(10));
       }
     }
   }
+  println!("Received everything!");
 }
