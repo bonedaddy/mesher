42148c9b1bb37b86844cc53f8a89e5d9ca7ab820
Nic Hartley <nic@cybers.eco>
2020-02-20 18:12:52 -0500

    docs: more module documentation

--- mesher/src/lib.rs
+++ mesher/src/lib.rs
@@ -7,10 +7,30 @@
 //! In smaller, more real words, mesher:
 //! 
 //! - Operates on mesh networks
-//! - Where nodes may be connected through a variety of channels (heterogeneous, not homogeneous)
+//! - Allows nodes to be connected through a variety of channels (heterogeneous, not homogeneous)
 //! - Prevents intermediate nodes from knowing the source or destination
+//! - Prevents intermediate nodes from knowing the contents of the message
 //! 
-//! It's primarily desigend for anonymous, one-way communication
+//! It's primarily desigend for anonymous, one-way communication.
+//! However, replies facilitate round-trip communications, which in turn can be used to make a tunnel.
+//! 
+//! For more information on the concepts underlying this library, see [the project repo's README](https://github.com/nic-hartley/mesher/blob/master/README.md).
+//! The rest of this API documentation assumes you've read and understood it.
+//! 
+//! The mesher API is fairly simple, and based around three pieces, which reflect the concepts described in the README:
+//! 
+//! - [`struct Mesher`](struct.Mesher.html) coordinates the rest of the objects, e.g. managing Transports, automatically handling bounces, etc.
+//! - [`trait Transport`](trait.Transport.html) defines the interface that `Mesher` uses to control Transports.
+//!   If you need them, e.g. for testing, there are debug transports available in [`mesher::debug_transports`](debug_transports/index.html).
+//! - [`struct Packet`](struct.Packet.html) makes building signed and unsigned packets easier.
+//! 
+//! Also worth mentioning are the types in [`mesher::crypto`](crypto/index.html), which encapsulate the manipulation of crypto primitives.
+//! You'll use them to pass keys into `Mesher` and `Packet`.
+//! They do offer secure keygen, but this crate **will not** handle storing keys for you, if you need that.
+//! 
+//! [`struct Message`](struct.Message.html) represents a message received.
+//! 
+//! There is, of course, a [`fail`](fail/index.html) module, with the expected [`enum MesherFail`](fail/enum.MesherFail.html) and [`type Result`](fail/type.Result.html) for this crate's error handling.
 
 // for transport::debug::InMemory
 #[macro_use]
